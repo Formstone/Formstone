6adae02a108a0fb486960439cf721a0f7ae5b2a5
Ben Plum <mr@benplum.com>
2014-11-01 19:09:22 -0400

    Working plugin factory :)

--- fs.core.js
+++ fs.core.js
@@ -1,30 +1,33 @@
-;(function ($, window, document, undefined) {
+
+var Formstone = (function ($, window, document, undefined) {
+
 	"use strict";
 
 	// Namespace
 
-	var $fs = {};
+	var Formstone = {};
 
-	window.Formstone = window.$fs = $fs;
+	window.Formstone = Formstone;
 
 	// Globals
 
-	$fs.$window              = null;
-	$fs.$document            = null;
-	$fs.$body                = null;
-	$fs.trueMobile           = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test((window.navigator.userAgent||window.navigator.vendor||window.opera));
-	$fs.transitionSupport    = true;
-	$fs.matchMediaSupport    = window.matchMedia;
+	Formstone.$window              = null;
+	Formstone.$document            = null;
+	Formstone.$body                = null;
+	Formstone.trueMobile           = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test((window.navigator.userAgent||window.navigator.vendor||window.opera));
+	Formstone.transitionSupport    = true;
+	Formstone.matchMediaSupport    = window.matchMedia;
 
 	// Classes
 
-	$fs.classes = {
+	Formstone.classes = {
 		base                 : "{ns}",
 		element              : "{ns}-element"
 	};
 
 	// Events
-	$fs.events = {
+
+	Formstone.events = {
 		click                : "click.{ns}",
 		dragEnter            : "dragenter.{ns}",
 		dragOver             : "dragover.{ns}",
@@ -50,34 +53,28 @@
 		touchStart           : "touchstart.{ns}",
 		touchMove            : "touchmove.{ns}",
 		touchEnd             : "touchend.{ns}",
-		// clickTouchStart      : $fs.events.click + " " + $fs.events.touchStart,
 		transition           : null
 	};
-
-	// Init Core
-
-	$.ready(function() {
-		$fs.$window = $(window);
-		$fs.$document = $(document);
-		$fs.$body = $("body");
-
-		$fs.events.transition = getTransitionEvent();
-	});
 
 	// Localize Classes
 
 	function localizeClasses(namespace, classes) {
-		var _classes = $.extend({}, $fs.classes, classes);
+		var _classes = {},
+			i;
 
-		for (var i in classes) {
+		classes = classes || {};
+
+		for (i in classes) {
 			if (classes.hasOwnProperty(i)) {
-				if ($fs.classes.hasOwnProperty(i)) {
-					// From Globals
-					_classes[i] = $fs.classes[ classes[i] ].replace("{ns}", namespace);
-				} else {
-					// Custom Classes
-					_classes[i] = namespace + "-" + classes[i];
-				}
+				// Custom Classes
+				_classes[ classes[i] ] = namespace + "-" + classes[i];
+			}
+		}
+
+		for (i in Formstone.classes) {
+			if (Formstone.classes.hasOwnProperty(i)) {
+				// From Globals
+				_classes[i] = Formstone.classes[i].replace("{ns}", namespace);
 			}
 		}
 
@@ -87,51 +84,52 @@
 	// Localize Events
 
 	function localizeEvents(namespace, events) {
-		var _events = $.extend({}, $fs.events, events);
+		var _events = {},
+			i;
 
-		for (var i in events) {
+		events = events || {};
+
+		for (i in events) {
 			if (events.hasOwnProperty(i)) {
-				if ($fs.events.hasOwnProperty(i)) {
-					// From Globals
-					_events[i] = $fs.events[ events[i] ].replace("{ns}", namespace);
-				} else {
-					// Custom Events
-					_events[i] = events[i] + "." + namespace;
-				}
+				// Custom Events
+				_events[ events[i] ] = events[i] + "." + namespace;
+			}
+		}
+
+		for (i in Formstone.events) {
+			if (Formstone.events.hasOwnProperty(i)) {
+				// From Globals
+				_events[i] = Formstone.events[i].replace("{ns}", namespace);
 			}
 		}
 
 		return _events;
 	}
 
-	// Widget Factory
+	// Plugin Bridge
 
-	$fs.widget = function(namespace, settings) {
+	Formstone.Plugin = function(namespace, settings) {
+		return (function(namespace, settings) {
+			// Locals
 
-		// Locals
-
-		var initialized = false,
-
-			defaults = settings.defaults,
+			settings.initialized = false;
 
 			// Localize Classes & Events
 
-			classes   = localizeClasses(namespace, settings.classes),
-			events    = localizeEvents(namespace, settings.events),
+			settings.classes   = localizeClasses(namespace, settings.classes);
+			settings.events    = localizeEvents(namespace, settings.events);
 
 			// Extend Internal Functions
 
-			functions = $.extend({
-
+			settings.functions = $.extend({
 				init         : $.noop,
 				construct    : $.noop,
 				destruct     : $.noop
-
-			}, settings.functions),
+			}, settings.functions);
 
 			// Extend Public Methods
 
-			methods = $.extend({
+			settings.methods = $.extend({
 
 				/**
 				 * @method
@@ -141,7 +139,7 @@
 				 * @example $.tipper("defaults", opts);
 				 */
 				defaults: function(options) {
-					defaults = $.extend(true, defaults, options || {});
+					settings.defaults = $.extend(true, settings.defaults, options || {});
 					return this;
 				},
 
@@ -157,7 +155,8 @@
 							data = getData($element);
 
 						if (data) {
-							functions.destruct.apply($element, data);
+							settings.functions.destruct.call($element, data);
+
 							$element.removeData(namespace);
 						}
 					});
@@ -165,110 +164,115 @@
 
 			}, settings.methods);
 
-		/**
-		 * @method private
-		 * @name init
-		 * @description Initializes plugin
-		 * @param opts [object] "Initialization options"
-		 */
-		function init(options) {
-			if (!initialized) {
-				initialized = true;
+			/**
+			 * @method private
+			 * @name init
+			 * @description Initializes plugin
+			 * @param opts [object] "Initialization options"
+			 */
+			function init(options) {
+				if (!settings.initialized) {
+					settings.initialized = true;
 
-				functions.init.apply(window);
+					settings.functions.init.call(window);
+				}
+
+				options = $.extend(true, {}, settings.defaults, options);
+
+				return this.each(function() {
+					var $element = $(this);
+
+					if (!getData($element)) {
+						var data = $.extend(true, {
+							$element: $element
+						}, options, $element.data(namespace + "-options"));
+
+						settings.functions.construct.call($element, data);
+
+						$element.addClass(settings.classes.element)
+						        .data(namespace, data);
+					}
+				});
 			}
 
-			options = $.extend(true, {}, defaults, options);
-
-			return this.each(function() {
-				build($(this), options);
-			});
-		}
-
-		/**
-		 * @method private
-		 * @name build
-		 * @description Builds each instance
-		 * @param $element [jQuery object] "Target jQuery object"
-		 * @param options [object] <{}> "Options object"
-		 */
-		function build($element, options) {
-			if (!getData($element)) {
-				var data = $.extend(true, {}, options, $element.data(namespace + "-options"));
-
-				functions.construct.apply($element, data).apply();
-
-				$element.addClass(classes.element)
-				        .data(namespace, data);
+			/**
+			 * @method private
+			 * @name getData
+			 * @description Create class selector from text
+			 * @param $element [object] "Target jQuery object"
+			 * @return [object] "Plugin data"
+			 */
+			function getData($element) {
+				return $element.data(namespace);
 			}
-		}
 
-		/**
-		 * @method private
-		 * @name getData
-		 * @description Create class selector from text
-		 * @param $element [object] "Target jQuery object"
-		 * @return [object] "Plugin data"
-		 */
-		function getData($element) {
-			return $element.data(namespace);
-		}
-
-		// Plugin Definition
-
-		$.fn[namespace] = function(method) {
-			if (methods[method]) {
-				return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-			} else if (typeof method === 'object' || !method) {
-				return init.apply(this, arguments);
+			/**
+			 * @method private
+			 * @name startTimer
+			 * @description Starts an internal timer
+			 * @param timer [int] "Timer ID"
+			 * @param time [int] "Time until execution"
+			 * @param callback [int] "Function to execute"
+			 */
+			function startTimer(timer, time, callback) {
+				clearTimer(timer);
+				return setTimeout(callback, time);
 			}
-			return this;
-		};
 
-		$[namespace] = function(method) {
-			if (method === "defaults") {
-				methods.defaults.apply(this, Array.prototype.slice.call(arguments, 1));
+			/**
+			 * @method private
+			 * @name clearTimer
+			 * @description Clears an internal timer
+			 * @param timer [int] "Timer ID"
+			 */
+			function clearTimer(timer) {
+				if (timer) {
+					clearTimeout(timer);
+					timer = null;
+				}
 			}
-		};
 
+			/**
+			 * @method private
+			 * @name getClassName
+			 * @description Create class selector from text
+			 * @param text [string] "Text to convert"
+			 * @return [string] "New class name"
+			 */
+			function getClassName(text) {
+				return "." + text;
+			}
+
+			// Plugin Definition
+
+			$.fn[namespace] = function(method) {
+				if (settings.methods[method]) {
+					// return settings.settings[method].apply(this, Array.prototype.slice.call(arguments, 1));
+
+					// Wrap public methods
+					return this.each(function() {
+						var $element = $(this),
+							data = getData($element);
+
+						if (data) {
+							settings.methods[method].apply($element, [data].concat(Array.prototype.slice.call(arguments, 1)));
+						}
+					});
+				} else if (typeof method === 'object' || !method) {
+					return init.apply(this, arguments);
+				}
+				return this;
+			};
+
+			$[namespace] = function(method) {
+				if (method === "fs") {
+					settings.methods.defaults.apply(this, Array.prototype.slice.call(arguments, 1));
+				}
+			};
+
+			return settings;
+		})(namespace, settings);
 	};
-
-	/**
-	 * @method private
-	 * @name startTimer
-	 * @description Starts an internal timer
-	 * @param timer [int] "Timer ID"
-	 * @param time [int] "Time until execution"
-	 * @param callback [int] "Function to execute"
-	 */
-	function startTimer(timer, time, callback) {
-		clearTimer(timer);
-		return setTimeout(callback, time);
-	}
-
-	/**
-	 * @method private
-	 * @name clearTimer
-	 * @description Clears an internal timer
-	 * @param timer [int] "Timer ID"
-	 */
-	function clearTimer(timer) {
-		if (timer) {
-			clearTimeout(timer);
-			timer = null;
-		}
-	}
-
-	/**
-	 * @method private
-	 * @name getClassName
-	 * @description Create class selector from text
-	 * @param text [string] "Text to convert"
-	 * @return [string] "New class name"
-	 */
-	function getClassName(text) {
-		return "." + text;
-	}
 
 	/**
 	 * @method private
@@ -295,54 +299,23 @@
 		// no transitions :(
 		if (!event) {
 			event = "transitionend";
-			$fs.transitionSupport = false;
+			Formstone.transitionSupport = false;
 		}
 
 		return event + ".{ns}";
 	}
 
-})(jQuery, window, document);
+	// Init Core
 
+	Formstone.$window      = $(window);
+	Formstone.$document    = $(document);
+	Formstone.$body        = $("body");
 
+	// Custom events
 
-// Usage ?
+	Formstone.events.transition         = getTransitionEvent();
+	Formstone.events.clickTouchStart    = Formstone.events.click + " " + Formstone.events.touchStart;
 
-/* global console */
-/* global classes */
-/* global events */
-
-;(function ($, window, document, undefined) {
-	var $fs = window.Formstone;
-
-	$fs.widget("tipper", {
-		classes: [
-			"active",
-			"visible"
-		],
-		// events: { },
-		functions: {
-
-			construct: function (data) {
-				return function() {
-					console.log("Construct", this, data);
-					console.log(classes);
-					console.log(events);
-				};
-			},
-
-			destruct: function (data) {
-				return function() {
-					console.log("Destruct", this, data);
-				};
-			}
-
-		},
-		methods: {}
-	});
+	return Formstone;
 
 })(jQuery, window, document);
-
-
-$(function() {
-	$("<a>").tipper();
-});
