95bf717c7851540cccdb5cf19d959a3f754109d4
Ben Plum <mr@benplum.com>
2014-12-22 15:24:10 -0500

    Clicks when panning

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -244,6 +244,7 @@
 			  .on(Events.pan, data, onPan)
 			  .on(Events.panEnd, data, onPanEnd)
 			  .on(Events.swipe, data, onSwipe)
+			  .on(Events.click, data, onClick)
 			  .css( Functions.prefix(TransitionProperty, "") );
 
 			resize.call(this, data);
@@ -665,7 +666,24 @@
 
 		position(data, index);
 
-		data.isSwiping  = false;
+		//data.isTouching = false;
+	}
+
+	/**
+	 * @method private
+	 * @name onClick
+	 * @description Cleans up touch interactions
+	 * @param data [object] "Instance data"
+	 * @param index [object] "New index"
+	 */
+
+	function onClick(e) {
+		var data = e.data;
+
+		if (data.isTouching) {
+			Functions.killEvent(e);
+		}
+
 		data.isTouching = false;
 	}
 
