619abf3c2e53049d581f68845165ffea07cfb926
Ben Plum <mr@benplum.com>
2015-09-10 10:55:05 -0400

    Adding link to changelog

--- tasks/docs.js
+++ tasks/docs.js
@@ -675,9 +675,10 @@ module.exports = function(grunt) {
 			markdown = '<a href="http://gruntjs.com" target="_blank"><img src="https://cdn.gruntjs.com/builtwith.png" alt="Built with Grunt"></a> \n' +
 					   '<a href="http://badge.fury.io/bo/formstone"><img src="https://badge.fury.io/bo/formstone.svg" alt="Bower version"></a> \n' +
 					   '<a href="https://travis-ci.org/Formstone/Formstone"><img src="https://travis-ci.org/Formstone/Formstone.svg?branch=master" alt="Travis CI"></a> \n\n' +
-					   '# ' + pkg.name + ' \n\n' +
+					   '# ' + pkg.realname + ' \n\n' +
 					   pkg.description + ' \n\n' +
-					   '[Documentation](docs/README.md)';
+					   '[Documentation](docs/README.md)' +
+					   '[Changelog](docs/CHANGELOG.md)';
 
 		grunt.file.write(destination, markdown);
 		grunt.log.writeln('File "' + destination + '" created.');
