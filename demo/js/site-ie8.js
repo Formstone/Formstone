d4cb55e57a301c36ecd5538e558fa0ada2fede65
Ben Plum <mr@benplum.com>
2016-01-14 17:33:55 -0500

    Light theme

--- demo/js/site-ie8.js
+++ demo/js/site-ie8.js
@@ -1,4 +1,4 @@
-/*! formstone v0.8.35 [site-ie8.js] 2016-01-09 | MIT License | formstone.it */
+/*! formstone v0.8.35 [site-ie8.js] 2016-01-14 | MIT License | formstone.it */
 
 /**
 * @preserve HTML5 Shiv 3.7.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
