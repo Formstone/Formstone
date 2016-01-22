4b161b097a5e3b7599737193644ba790006d1933
Ben Plum <mr@benplum.com>
2016-01-22 16:42:34 -0500

    Starting lightbox light theme

--- demo/js/site-ie8.js
+++ demo/js/site-ie8.js
@@ -1,4 +1,4 @@
-/*! formstone v0.8.36 [site-ie8.js] 2016-01-15 | MIT License | formstone.it */
+/*! formstone v0.8.36 [site-ie8.js] 2016-01-22 | MIT License | formstone.it */
 
 /**
 * @preserve HTML5 Shiv 3.7.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
