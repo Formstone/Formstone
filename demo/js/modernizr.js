d6ec2f32f3db450c5e915d5ed40549b7fbf46225
Ben Plum <mr@benplum.com>
2016-05-07 13:18:12 -0400

    Starting tabbed demo styles

--- demo/js/modernizr.js
+++ demo/js/modernizr.js
@@ -1,5 +1,97 @@
 /*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
 
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
+
 /*! formstone v1.1.0 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
 
 /*! formstone v1.1.0 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
--- demo/js/src/modules/page.js
+++ demo/js/src/modules/page.js
@@ -4,6 +4,23 @@
 
 	/* global picturefill */
 
+/*
+	// https://github.com/angular/angular.js/blob/v1.3.14/src/ngSanitize/sanitize.js#L435
+	function encodeEntities(value) {
+		return value.replace(/&/g, '&amp;')
+					.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, function(value) {
+						var hi = value.charCodeAt(0);
+						var low = value.charCodeAt(1);
+						return '&#' + (((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000) + ';';
+					})
+					.replace(/([^\#-~| |!])/g, function(value) {
+						return '&#' + value.charCodeAt(0) + ';';
+					})
+					.replace(/</g, '&lt;')
+					.replace(/>/g, '&gt;');
+	}
+*/
+
 	Site.modules.Page = (function($, Site) {
 
 		function init() {
@@ -18,8 +35,34 @@
 				path: '/'
 			});
 
-			buildPlugins("fs-light");
+			$(".demo_container").each(function(index) {
+				var html  = "",
+					$demo = $(this),
+					$example = $demo.find(".demo_example"),
+					$code    = $demo.find(".demo_code");
 
+				$example.attr("id", "example-" + index);
+				$code.attr("id", "code-" + index);
+
+				html += '<div class="demo_tabs contain">';
+				html += '<a href="#example-' + index + '" class="demo_tab js-demo_tabs" data-tabs-group="demo-' + index + '">Demo</a>';
+				html += '<a href="#code-' + index +    '" class="demo_tab js-demo_tabs" data-tabs-group="demo-' + index + '">Code</a>';
+				html += '</div>';
+
+				$demo.prepend(html);
+			});
+
+
+/*
+			$(".js-demo").each(function() {
+				var $demo = $(this),
+					content = encodeEntities($demo.html().trim());
+
+				$demo.after('<pre>' + content + '</pre>');
+			});
+*/
+
+			buildPlugins("fs-light");
 
 			Site.onScroll.push(scroll);
 			Site.onResize.push(resize);
@@ -63,6 +106,8 @@
 			Site.$body.find(".js-tooltip").tooltip("destroy");
 			Site.$body.find(".js-upload").upload("destroy");
 
+			Site.$body.find(".js-demo_tabs").off("update.tabs").tabs("destroy");
+
 			// Init
 
 			Site.$body.find(".js-background").background(options);
@@ -80,6 +125,33 @@
 			Site.$body.find(".js-tabs").tabs(options);
 			Site.$body.find(".js-tooltip").tooltip(options);
 			Site.$body.find(".js-upload").upload(options);
+
+			Site.$body.find(".js-demo_tabs").tabs({
+				mobileMaxWidth: "0px"
+			}).on("update.tabs", resetPlugins);
+		}
+
+		function resetPlugins() {
+			var $tab = $(this),
+				$content = $( $tab.attr("href") );
+
+			$content.find(".js-background").background("resize");
+			$content.find(".js-carousel").carousel("resize");
+/*
+			$content.find(".js-checkbox, .js-radio, input[type=checkbox], input[type=radio]").checkbox("destroy");
+			$content.find(".js-dropdown").dropdown("destroy");
+			$content.find(".js-equalize").equalize("destroy");
+			$content.find(".js-lightbox").lightbox("destroy");
+			$content.find(".js-navigation").navigation("destroy");
+			$content.find("input[type=number]").number("destroy");
+			$content.find(".js-pagination").pagination("destroy");
+			$content.find("input[type=range]").range("destroy");
+			$content.find(".js-scrollbar").scrollbar("destroy");
+			$content.find(".js-swap").swap("destroy");
+			$content.find(".js-tabs").tabs("destroy");
+			$content.find(".js-tooltip").tooltip("destroy");
+			$content.find(".js-upload").upload("destroy");
+*/
 		}
 
 		/* Hook Into Main init Routine */
