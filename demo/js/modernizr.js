416314221ecce2890a3ffd4259e9b492a149ac58
Ben Plum <mr@benplum.com>
2016-05-15 10:36:18 -0400

    Demo styles

--- demo/js/modernizr.js
+++ demo/js/modernizr.js
@@ -2,6 +2,18 @@
 
 /*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
 
+/*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
+
+/*! formstone v1.1.1 [modernizr.js] 2016-05-15 | GPL-3.0 License | formstone.it */
+
 /*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
 
 /*! formstone v1.1.1 [modernizr.js] 2016-05-07 | GPL-3.0 License | formstone.it */
