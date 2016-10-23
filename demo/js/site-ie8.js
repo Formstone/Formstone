fcff939b6d2ee1fb4e6ce3ba8210b40b9cddf2d9
Ben Plum <mr@benplum.com>
2016-10-23 18:51:35 -0400

    Version bump

--- demo/js/site-ie8.js
+++ demo/js/site-ie8.js
@@ -1,4 +1,4 @@
-/*! formstone v1.2.2 [site-ie8.js] 2016-10-23 | GPL-3.0 License | formstone.it */
+/*! formstone v1.3.0 [site-ie8.js] 2016-10-23 | GPL-3.0 License | formstone.it */
 
 /**
 * @preserve HTML5 Shiv 3.7.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
