efa14d52c982ebd23e3a0ca497035f887bac868b
Ben Plum <mr@benplum.com>
2017-08-17 14:39:42 -0400

    Gulp styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp');
 var header       = require('gulp-header');
 var less         = require('gulp-less');
+var clip         = require('gulp-clip-empty-files');
 var cleanCSS     = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var modernizr    = require('gulp-modernizr');
@@ -16,20 +17,25 @@ comment += ' <%= pkg.license %> License | <%= pkg.homepage_short %> */\n';
 
 // Less
 gulp.task('styles', function() {
-  return gulp.src('./assets/less/*.less')
+  return gulp.src(['./src/less/**/*.less', '!./src/less/imports/*'])
     .pipe(less())
     .pipe(autoprefixer({
-    	browsers: ['last 2 versions']
+    	browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie >= 10']
     }))
     .pipe(cleanCSS({
     	compatibility: 'ie10'
     }))
-    .pipe(gulp.dest('./public/css'));
+	.pipe(clip())
+	.pipe(header(comment, {
+		pkg:  pkg,
+		date: date
+	}))
+    .pipe(gulp.dest('./dist/css'));
 });
 
 // JS
 gulp.task('scripts', function() {
-  gulp.src('./src/js/*')
+  gulp.src('./src/js/*.js')
     .pipe(include())
     .pipe(uglify())
 	.pipe(header(comment, {
