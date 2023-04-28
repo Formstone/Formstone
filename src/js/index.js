536c59b6453b113ddb151c970c00f9fd10f5bd52
Ben Plum <mr@benplum.com>
2023-04-28 14:08:21 -0400

    Fixing index

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import CheckPoint from './checkpoint.js';
 import Cookie from './cookie.js';
 import Lightbox from './lightbox.js';
 import MediaQuery from './mediaquery.js';
-import Navigation from './navigation';
+import Navigation from './navigation.js';
 import Swap from './swap.js';
 import Tabs from './tabs.js';
 
